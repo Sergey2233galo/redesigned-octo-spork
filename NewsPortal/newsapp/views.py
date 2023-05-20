adf90c6e5dc87db816cc8c6a24c428a3d7c4149b
Sergey2233galo <126077067+Sergey2233galo@users.noreply.github.com>
2023-05-20 05:30:13 +0500

    Add files via upload

---
+++ NewsPortal/newsapp/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Post
+from .filters import PostFilter
+from datetime import datetime
+from .forms import PostForm
+from django.urls import reverse_lazy
+
+class PostList(ListView):
+    model = Post
+    ordering = '-id'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_search.html'
+    ordering = '-id'
+    paginate_by = 15
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
